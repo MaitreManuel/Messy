090f40089dded85fa1ac16c6fbba0849b9aeec36
trystan.eveno <trystan.eveno@gmail.com>
2017-02-19 22:17:19 +0100

    remove location.reload when new message, now work without refresh

--- build/application.js
+++ build/application.js
@@ -21933,7 +21933,34 @@
 	    },
 
 	    newMessage: function () {
+	        var me = this;
+
 	        openOverlay(true);
+	        $('#newMessage').click(function () {
+	            var url = 'https://tpiut2017.cleverapps.io',
+	                myMessage = JSON.stringify({
+	                'message': document.getElementById('message').value
+	            });
+
+	            spin(true);
+	            fetch(url + '/u/timeline', {
+	                method: 'POST',
+	                headers: {
+	                    'Accept': 'application/json',
+	                    'Content-Type': 'application/json',
+	                    'Content-Length': myMessage.length,
+	                    'Authorization': 'Bearer:' + sessionStorage.getItem("token")
+	                },
+	                body: myMessage
+	            }).then(function (reponse) {
+	                toastr.success('', 'Message Publié');
+	                me.getMessages();
+	            }).catch(function (err) {
+	                toastr.error('Une erreur est survenue', 'Message Non Publié');
+	                spin(false);
+	                console.log(err);
+	            });
+	        });
 	    },
 
 	    render: function () {
